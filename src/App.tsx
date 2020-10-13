be3f47bd7d8d1b04ecfe346b9cf101cbcc5398fb
Matheus Ribeiro <mathribe2020@gmail.com>
2020-10-13 00:25:57 -0300

    Configure the landing page & orphanages map routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
 import React from "react";
-import { FiArrowRight } from "react-icons/fi";
 
 import "./styles/global.css";
-import "./styles/pages/landing.css";
-import logoimg from "./images/happy_logo.svg";
-// import kidsimg from "./images/landing_kids.svg";
+
+import Routes from "./routes";
+
 function App() {
-	return (
-		<div id="page-landing">
-			<div className="content-wrapper">
-				<img src={logoimg} alt="Happy" />
-				<main>
-					<h1>Leve felicidade para o mundo</h1>
-					<p>Visite orfanatos e mude o dia de muitas crianças.</p>
-				</main>
-
-				<div className="location">
-					<strong>Igaraçu do Tietê</strong>
-					<span>São Paulo</span>
-				</div>
-
-				<a href="" className="enter-app">
-					<FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-				</a>
-			</div>
-		</div>
-	);
+	return <Routes />;
 }
 
 export default App;
