b1a2639e5d96ba947afde7c199f8a94e92877638
Matheus Ribeiro <mathribe2020@gmail.com>
2020-10-12 23:31:04 -0300

    Create the landing page, add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+/*
+  const render(element, container)
+  -> element: <App/>
+  -> container: <element id="root">
+*/
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
