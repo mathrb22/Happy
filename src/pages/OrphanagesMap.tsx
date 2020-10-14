b04d604ed4ff31924657a29445ed0abbed3f07a4
Matheus Ribeiro <mathribe2020@gmail.com>
2020-10-14 13:12:49 -0300

    Add the OrphanagesMap page and stylesheet

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiPlus } from "react-icons/fi";
+import mapMarkerImg from "../images/map_marker.svg";
+import "../styles/pages/orphanages-map.css";
 
 function OrphanagesMap() {
-	return <h1>Hello World</h1>;
+	return (
+		<div id="page-map">
+			<aside>
+				<header>
+					<img src={mapMarkerImg} alt="Happy" />
+					<h2>Escolha um orfanato no mapa</h2>
+					<p>Muitas crianças estão esperando a sua visita :)</p>
+				</header>
+
+				<footer>
+					<strong>Igaraçu do Tietê</strong>
+					<span>São Paulo</span>
+				</footer>
+			</aside>
+
+			<div></div>
+
+			<Link to="" className="create-orphanage">
+				<FiPlus size={32} color="#FFF" />
+			</Link>
+		</div>
+	);
 }
 
 export default OrphanagesMap;
