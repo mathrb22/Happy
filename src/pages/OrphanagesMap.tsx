138a44ffdbb7db5c3eb9d543f0a12d2eef82e61a
Matheus Ribeiro <mathribe2020@gmail.com>
2020-10-14 22:35:08 -0300

    Add & style the Map components from React Leaflet

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
+import { Map, TileLayer } from "react-leaflet";
+//Tile layer -> fonte de blocos de dados de imagens; servidor para requisições de imagens para preencher a área de visualização do mapa
+import "leaflet/dist/leaflet.css";
+
 import mapMarkerImg from "../images/map_marker.svg";
 import "../styles/pages/orphanages-map.css";
 
@@ -20,7 +24,15 @@ function OrphanagesMap() {
 				</footer>
 			</aside>
 
-			<div></div>
+			<Map
+				center={[-22.5025352, -48.560993]}
+				zoom={14}
+				style={{ width: "100%", height: "100%" }}>
+				{/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+				<TileLayer
+					url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+				/>
+			</Map>
 
 			<Link to="" className="create-orphanage">
 				<FiPlus size={32} color="#FFF" />
