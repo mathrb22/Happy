5a52c6d3424f3d8d26b26a51267db96c7f5fd6a6
Matheus Ribeiro <mathribe2020@gmail.com>
2020-10-17 21:57:03 -0300

    Create the index() method to find the entities

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -4,6 +4,14 @@ import Orphanage from "../models/Orphanage";
 
 //Exportando o objeto OrphanagesController:
 export default {
+	async index(request: Request, response: Response) {
+		const orphanagesRepository = getRepository(Orphanage);
+
+		const orphanages = await orphanagesRepository.find();
+
+		return response.json(orphanages);
+	},
+
 	async create(request: Request, response: Response) {
 		//Desestruturando o corpo da requisição:
 		const {
@@ -15,7 +23,7 @@ export default {
 			opening_hours,
 			open_on_weekends,
 		} = request.body;
-
+      
 		const orphanagesRepository = getRepository(Orphanage);
 
 		const orphanage = orphanagesRepository.create({
