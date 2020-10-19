c632d96b25e842e7a5895767a87c244e29965b25
Matheus Ribeiro <mathribe2020@gmail.com>
2020-10-19 01:41:52 -0300

    Create an error handler for the API requests

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import path from "path";
+import "express-async-errors";
 import "./database/connection";
 import routes from "./routes";
+import errorHandler from "./errors/handler";
 
 const app = express();
 
@@ -10,5 +12,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+
+app.use(errorHandler);
 
 app.listen(3333);
